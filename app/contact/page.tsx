24b169281da2d70aaf290cee52c4b275a97de21d
emirrtopaloglu <emirrttopaloglu@gmail.com>
2023-09-21 23:34:35 +0300

    added contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,43 +1,61 @@
 import PageHeader from "@/components/layout/web/page-header";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import Link from "next/link";
 
-export default function AboutPage() {
+export default function ContactPage() {
   return (
     <>
       <PageHeader>
-        <h1 className="text-4xl leading-relaxed text-stone-800">About Us</h1>
+        <h1 className="text-4xl leading-relaxed text-stone-800">
+          Contact Page
+        </h1>
         <p className="text-base leading-relaxed text-stone-500 max-w-[60%]">
-          We are a team of passionate people whose goal is to improve everyone's
-          life through disruptive products. We build great products to solve
-          your business problems.
+          You can easily contact us by filling out the form below.
         </p>
       </PageHeader>
-      <div className="container py-16 max-w-[65%] text-stone-700 leading-relaxed text-base space-y-8">
-        <p>
-          Connect your conversations with the tools and services that you use to
-          get the job done. With over 1,500 apps and a robust API, the Slack
-          platform team works with partners and developers globally to build
-          apps and integrations that streamline your work, automate mundane
-          tasks and bring context into your conversations in Voip.
-        </p>
-        <h2>Our Mission</h2>
-        <p>
-          We are a team of passionate people whose goal is to improve everyone's
-          life through disruptive products. We build great products to solve
-          your business problems. Our products are designed for small to medium
-          size companies willing to optimize their performance.
-        </p>
-        <h3>Join Us and Enjoy</h3>
-        <p>
-          We are a team of passionate people whose goal is to improve everyone's
-          life through disruptive products. We build great products to solve
-          your business problems. Our products are designed for small to medium
-          size companies willing to optimize their performance. Connect your
-          conversations with the tools and services that you use to get the job
-          done. With over 1,500 apps and a robust API, the Slack platform team
-          works with partners and developers globally to build apps and
-          integrations that streamline your work, automate mundane tasks and
-          bring context into your conversations in Voip.
-        </p>
+      <div className="container py-16 max-w-[65%] text-stone-700 leading-relaxed text-base space-y-4">
+        <div className="flex space-x-8">
+          <div className="flex-1">
+            <Label htmlFor="name">Your Name *</Label>
+            <Input id="name" type="text" placeholder="Name" className="h-12" />
+          </div>
+          <div className="flex-1">
+            <Label htmlFor="email">Contact Email *</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="Email address"
+              className="h-12"
+            />
+          </div>
+        </div>
+        <div className="flex">
+          <div className="flex-1">
+            <Label htmlFor="message">Message *</Label>
+            <Textarea
+              id="message"
+              placeholder="Your message"
+              className="h-32"
+            />
+          </div>
+        </div>
+        <div className="flex">
+          <p className="text-base text-stone-500 leading-loose">
+            By submitting this form you agree to our terms and conditions and
+            our{" "}
+            <Link href="/privacy-policy" className="text-blue-500">
+              Privacy Policy
+            </Link>{" "}
+            which explains how we may collect, use and disclose your personal
+            information including to third parties.
+          </p>
+        </div>
+        <div className="flex">
+          <Button className="h-12">Send Message</Button>
+        </div>
       </div>
     </>
   );
