6affad3394cbd3125cbf06328a718fbb1efdfaf8
emirrtopaloglu <emirrttopaloglu@gmail.com>
2023-09-22 23:11:16 +0300

    fix images

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -7,6 +7,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Image from "next/image";
 
 export default function BlogPage() {
   return (
@@ -17,11 +18,13 @@ export default function BlogPage() {
       <div className="container space-y-8 my-8">
         <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {Array.from({ length: 6 }).map((_, index) => (
-            <Card>
+            <Card key={index}>
               <CardHeader>
-                <img
+                <Image
                   src="https://plus.unsplash.com/premium_photo-1695339147014-32f68336c10c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
                   className="w-full lg:h-64 md:h-48 object-cover"
+                  width="720"
+                  height="480"
                   alt={"cover-image-" + index}
                 />
               </CardHeader>
--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
